b3cbbe7bfaa939b2f50cfb2080b1bbfc234b8b7d
svitorz <vitor.fabio.castro@gmail.com>
2025-01-22 00:41:18 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full bg-black">
+        <nav className="flex justify-center items-center bg-gray-800">
+          <div className="rounded p-2">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default App;
